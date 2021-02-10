239561ecce3242be97ebdb55be0c988aababe9b7
Richard Bodsworth <richardbodsworth@gmail.com>
2021-02-10 15:01:15 +0000

    Folder setup, Classes created, tests passing for manager and employee

---
+++ src/main/java/staff/Employee.java
@@ -0,0 +1,37 @@
+package staff;
+
+public abstract class Employee {
+
+    private String name;
+    private String ni;
+    private double salary;
+
+    public Employee(String name, String ni, double salary) {
+        this.name = name;
+        this.ni = ni;
+        this.salary = salary;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getNi(){
+        return this.ni;
+
+    }
+    public double getSalary(){
+        return this.salary;
+
+    }
+
+    public void raiseSalary(double raiseAmount){
+        this.salary += raiseAmount;
+    }
+
+    public double payBonus(){
+        return salary * 0.01;
+    }
+
+
+}
---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,37 @@
+import management.Manager;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ManagerTest {
+
+    Manager manager;
+
+    @Before
+    public void before(){
+        manager = new Manager("Mr Burns", "KL271576B", 100, "Operations");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Mr Burns", manager.getName());
+    }
+
+    @Test
+    public void hasNi(){
+        assertEquals("KL271576B", manager.getNi());
+    }
+
+    @Test
+    public void hasSalary(){
+        assertEquals(100, manager.getSalary(), 0.01);
+    }
+
+    @Test
+    public void raiseSalary(){
+        manager.raiseSalary(50);
+        assertEquals(150, manager.getSalary(), 0.01);
+    }
+
+}
